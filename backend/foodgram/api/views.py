3ca308ded0c1c52e4d79ec07f6ce74bfcc0758b2
vladislav-vorobyeff <vlad_vorobyeff@mail.ru>
2023-08-20 19:48:12 +0500

    try13123

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -84,9 +84,8 @@ class RecipeViewSet(ModelViewSet):
 
         ingredients = IngredientRecipe.objects.filter(
             recipe__shopping_cart__user=request.user).values(
-                'ingredient__name', 'ingredient__measurement_unit').annotate(
-                    amount=Sum('amount')
-        )
+                'ingredient__name',
+                'ingredient__measurement_unit').annotate(amount=Sum('amount'))
 
         today = datetime.today()
         content = (
