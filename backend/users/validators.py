b96c8e75edaab636c236a667a60650a3830c9900
vladislav-vorobyeff <vlad_vorobyeff@mail.ru>
2023-08-25 01:50:01 +0500

    rev24

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -3,4 +3,4 @@ from django.core.exceptions import ValidationError
 
 def validate_username_me(value):
     if 'me' in value.lower():
-        raise ValidationError('me использовать недопустимо')
+        raise ValidationError('me использовать нельзя')
