ebbebd37cf935bfa6ab6d751c040a1e4504c7f92
vladislav-vorobyeff <vlad_vorobyeff@mail.ru>
2023-08-26 18:21:44 +0500

    filshcart

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -28,7 +28,7 @@ class RecipeFilter(FilterSet):
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
-            return queryset.filter(shopping_carts__user=self.request.user)
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
 
     def get_is_favorited(self, queryset, name, value):
