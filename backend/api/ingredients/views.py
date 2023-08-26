eeeb9047e4edc06541678ce0f1f9151399cdc3b8
vladislav-vorobyeff <vlad_vorobyeff@mail.ru>
2023-08-26 16:38:31 +0500

    1638

--- backend/api/ingredients/views.py
+++ backend/api/ingredients/views.py
@@ -1,4 +1,3 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from ingredients.models import Ingredient
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -9,5 +8,6 @@ from .serializers import IngredientSerializer
 class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = IngredientFilter
+    pagination_class = None
+    filter_backends = (IngredientFilter,)
+    search_fields = ('^name',)
