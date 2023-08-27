807ff2047a976c024674c29550962fa5c589d22f
vladislav-vorobyeff <vlad_vorobyeff@mail.ru>
2023-08-28 04:44:10 +0500

    try4:44

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,11 +1,7 @@
 import csv
-import os
+from django.core.management.base import BaseCommand
 
-from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
 from recipes.models import Ingredient
-
-DATA_ROOT = os.path.join(settings.BASE_DIR)
 
 
 class Command(BaseCommand):
@@ -15,20 +11,16 @@ class Command(BaseCommand):
         parser.add_argument('filename', default='ingredients.csv', nargs='?',
                             type=str)
 
-    def handle(self, *args, **options):
-        try:
-            with open(
-                os.path.join(DATA_ROOT, options['filename']),
-                newline='',
-                encoding='utf8'
-            ) as csv_file:
-                data = csv.reader(csv_file)
-                Ingredient.objects.bulk_create(
-                    (Ingredient(
-                        name=row[0],
-                        measurement_unit=row[1])
-                        for row in data),
-                    batch_size=999
-                )
-        except FileNotFoundError:
-            raise CommandError(f'Файл "{options["filename"]}" не найден')
+    def handle(self, *args):
+        with open(
+            'foodgram/data/ingredients.csv',
+            encoding='utf8'
+        ) as csv_file:
+            data = csv.reader(csv_file)
+            Ingredient.objects.bulk_create(
+                (Ingredient(
+                    name=row[0],
+                    measurement_unit=row[1])
+                    for row in data),
+                batch_size=999
+            )
