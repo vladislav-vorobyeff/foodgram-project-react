be45dd4c2b00bd3624a9258f1b93819b9de85e7f
vladislav-vorobyeff <vlad_vorobyeff@mail.ru>
2023-08-28 05:23:57 +0500

    data

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,7 +1,11 @@
 import csv
-from django.core.management.base import BaseCommand
+import os
 
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
 from recipes.models import Ingredient
+
+DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
 
 
 class Command(BaseCommand):
@@ -12,15 +16,19 @@ class Command(BaseCommand):
                             type=str)
 
     def handle(self, *args, **options):
-        with open(
-            'foodgram/data/ingredients.csv',
-            encoding='utf8'
-        ) as csv_file:
-            data = csv.reader(csv_file)
-            Ingredient.objects.bulk_create(
-                (Ingredient(
-                    name=row[0],
-                    measurement_unit=row[1])
-                    for row in data),
-                batch_size=999
-            )
+        try:
+            with open(
+                os.path.join(DATA_ROOT, '..', 'data', options['filename']),
+                newline='',
+                encoding='utf8'
+            ) as csv_file:
+                data = csv.reader(csv_file)
+                Ingredient.objects.bulk_create(
+                    (Ingredient(
+                        name=row[0],
+                        measurement_unit=row[1])
+                        for row in data),
+                    batch_size=999
+                )
+        except FileNotFoundError:
+            raise CommandError('Файл ingredients.csv не найден')
